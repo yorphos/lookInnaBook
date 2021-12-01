abe54e3896c63db83cf161f55cb682e30b1e5ca5
Steven Pham <steven.pham.8.2000@gmail.com>
2021-12-01 03:27:56 -0500

    Add database schema and MVP for getting book titles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+mod schema;
+
+use rocket_sync_db_pools::{database, postgres};
+
+#[database("postgres")]
+struct DbConn(postgres::Client);
+
+#[get("/books")]
+async fn books(conn: DbConn) -> String {
+    let mut result = String::new();
+    if let Ok(v) = conn.run(|c| c.query("SELECT * FROM base.book", &[])).await {
+        for row in v {
+            result += &row.get::<&str, String>("title");
+            result += "\n";
+        }
+    }
+
+    result
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![books])
+        .attach(DbConn::fairing())
 }
