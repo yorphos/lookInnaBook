26285b6c643e86c14451f617f09d8d16326b9f9f
Steven Pham <steven.pham.8.2000@gmail.com>
2021-12-10 21:46:49 -0500

    Update sql schema dump

--- schema-backup.sql
+++ schema-backup.sql
@@ -67,9 +67,12 @@ CREATE TABLE base.book (
     genre character varying(20),
     publisher_id integer,
     num_pages integer,
-    price money,
+    price numeric(6,2),
     author_royalties numeric(3,2),
-    reorder_threshold integer
+    reorder_threshold integer,
+    title character varying(50),
+    stock integer,
+    discontinued boolean
 );
 
 
@@ -110,7 +113,6 @@ CREATE TABLE base.customer (
     name character varying(20),
     email character varying(20),
     password_hash character(60),
-    password_salt bytea,
     default_shipping_address integer,
     default_payment_info_id integer
 );
@@ -269,7 +271,8 @@ CREATE TABLE base.publisher (
     company_name character varying(20),
     phone_number character varying(20),
     bank_number character varying(20),
-    address_id integer
+    address_id integer,
+    email character varying(30)
 );
 
 
@@ -288,6 +291,37 @@ ALTER TABLE base.publisher ALTER COLUMN publisher_id ADD GENERATED ALWAYS AS IDE
     CACHE 1
 );
 
+
+--
+-- Name: raw_sales_data; Type: VIEW; Schema: base; Owner: steven
+--
+
+CREATE VIEW base.raw_sales_data AS
+ SELECT in_order.isbn,
+    orders.order_id,
+    orders.customer_id,
+    orders.shipping_address_id,
+    orders.tracking_number,
+    orders.order_status,
+    orders.order_date,
+    orders.payment_info_id,
+    in_order.quantity,
+    book.author_name,
+    book.genre,
+    book.publisher_id,
+    book.num_pages,
+    book.price,
+    book.author_royalties,
+    book.reorder_threshold,
+    book.title,
+    book.stock,
+    book.discontinued
+   FROM ((base.orders
+     JOIN base.in_order USING (order_id))
+     JOIN base.book USING (isbn));
+
+
+ALTER TABLE base.raw_sales_data OWNER TO steven;
 
 --
 -- Name: restock_order; Type: TABLE; Schema: base; Owner: steven
@@ -320,10 +354,38 @@ ALTER TABLE base.restock_order ALTER COLUMN restock_order_id ADD GENERATED ALWAY
 
 
 --
+-- Name: sales; Type: VIEW; Schema: base; Owner: steven
+--
+
+CREATE VIEW base.sales AS
+ SELECT orders.order_date,
+    sum(in_order.quantity) AS quantity
+   FROM ((base.orders
+     JOIN base.in_order USING (order_id))
+     JOIN base.book USING (isbn))
+  GROUP BY orders.order_date;
+
+
+ALTER TABLE base.sales OWNER TO steven;
+
+--
 -- Data for Name: address; Type: TABLE DATA; Schema: base; Owner: steven
 --
 
 COPY base.address (address_id, street_address, postal_code, province) FROM stdin;
+1	12 Main St	K2W0A8	ON
+2	777 U St.	K1S189	ON
+3	12831 W	1231	IW
+4	777 U St.	K1S189	ON
+5	12831 W	1231	IW
+6	777 U St.	K1S189	ON
+7	12831 W	1231	IW
+8	777 U St.	K1S189	ON
+9	12831 W	1231	IW
+10	777 U St.	K1S189	ON
+11	12831 W	1231	IW
+12	732 Main St	K192W9	ON
+13	829 Recipe Avenue	FOOD	ON
 \.
 
 
@@ -331,7 +393,11 @@ COPY base.address (address_id, street_address, postal_code, province) FROM stdin
 -- Data for Name: book; Type: TABLE DATA; Schema: base; Owner: steven
 --
 
-COPY base.book (isbn, author_name, genre, publisher_id, num_pages, price, author_royalties, reorder_threshold) FROM stdin;
+COPY base.book (isbn, author_name, genre, publisher_id, num_pages, price, author_royalties, reorder_threshold, title, stock, discontinued) FROM stdin;
+82381902	B. Kenny	Sci-Fi	2	300	29.99	0.10	10	Jarjar Journeys	29	f
+82381967	B. BKenny	Sci-Fi	2	300	29.99	0.10	10	Jarjar Journeys 2	24	f
+82381990	B. BKenny	Sci-Fi	2	300	29.99	0.10	10	Jarjar Journeys 3: Jarjar Redux Complete	28	f
+312321	John Shefman	Cooking	4	25	50.00	0.10	5	Food For People Who Eat	7	f
 \.
 
 
@@ -347,7 +413,9 @@ COPY base.book_collection (collection_id, curator_owner_id) FROM stdin;
 -- Data for Name: customer; Type: TABLE DATA; Schema: base; Owner: steven
 --
 
-COPY base.customer (customer_id, name, email, password_hash, password_salt, default_shipping_address, default_payment_info_id) FROM stdin;
+COPY base.customer (customer_id, name, email, password_hash, default_shipping_address, default_payment_info_id) FROM stdin;
+2	Steve	test@local	$2b$10$BXeln024yC2PhT3J9jpuL.OYB9ciEUm0Lgvt3pDpTY0GlICWJGO4q	8	2
+3	Steve	test2@local	$2b$10$27lLyoOAxeJBQlGbh0ZL0eFZ80VwquejFQD1ecygB3dbsTkokb9sS	10	3
 \.
 
 
@@ -372,6 +440,16 @@ COPY base.in_collection (collection_id, isbn) FROM stdin;
 --
 
 COPY base.in_order (isbn, order_id, quantity) FROM stdin;
+82381967	3	2
+82381990	3	1
+82381967	4	1
+82381967	5	1
+82381902	6	1
+82381967	7	1
+82381990	8	1
+82381990	9	1
+82381967	10	4
+312321	11	3
 \.
 
 
@@ -380,6 +458,16 @@ COPY base.in_order (isbn, order_id, quantity) FROM stdin;
 --
 
 COPY base.orders (order_id, customer_id, shipping_address_id, tracking_number, order_status, order_date, payment_info_id) FROM stdin;
+2	3	10	953146797	PR	2021-12-08	3
+3	3	10	661630750	PR	2021-12-08	3
+4	3	10	1290554229	PR	2021-12-08	3
+5	3	10	3305752897	PR	2021-12-08	3
+6	3	10	368815587	PR	2021-12-08	3
+7	3	10	1887640785	PR	2021-12-08	3
+8	3	10	1118788729	PR	2021-12-08	3
+9	3	10	1910357174	PR	2021-12-09	3
+10	3	10	2235082141	PR	2021-12-09	3
+11	3	10	953908047	PR	2021-12-10	3
 \.
 
 
@@ -396,6 +484,9 @@ COPY base.owner (owner_id, name, email, password_hash, password_salt) FROM stdin
 --
 
 COPY base.payment_info (payment_info_id, name_on_card, expiry, card_number, cvv, billing_address_id) FROM stdin;
+1	Steven Pham	1/23	12312313	199	7
+2	Steven Pham	1/23	12312313	199	9
+3	Steven Pham	1/23	12312313	199	11
 \.
 
 
@@ -403,7 +494,10 @@ COPY base.payment_info (payment_info_id, name_on_card, expiry, card_number, cvv,
 -- Data for Name: publisher; Type: TABLE DATA; Schema: base; Owner: steven
 --
 
-COPY base.publisher (publisher_id, company_name, phone_number, bank_number, address_id) FROM stdin;
+COPY base.publisher (publisher_id, company_name, phone_number, bank_number, address_id, email) FROM stdin;
+2	ABC Books	888-999-0984	772823	1	publishing@abcbooks.com
+3	CDE Books	613772222	77282890283901	12	publisher@cdebooks.com
+4	Food Incorporated	2223334444	723819372193	13	publishing@foodcorp.com
 \.
 
 
@@ -419,7 +513,7 @@ COPY base.restock_order (restock_order_id, isbn, quantity, price_per_unit, order
 -- Name: address_address_id_seq; Type: SEQUENCE SET; Schema: base; Owner: steven
 --
 
-SELECT pg_catalog.setval('base.address_address_id_seq', 1, false);
+SELECT pg_catalog.setval('base.address_address_id_seq', 13, true);
 
 
 --
@@ -433,14 +527,14 @@ SELECT pg_catalog.setval('base.book_collection_collection_id_seq', 1, false);
 -- Name: customer_customer_id_seq; Type: SEQUENCE SET; Schema: base; Owner: steven
 --
 
-SELECT pg_catalog.setval('base.customer_customer_id_seq', 1, false);
+SELECT pg_catalog.setval('base.customer_customer_id_seq', 3, true);
 
 
 --
 -- Name: orders_order_id_seq; Type: SEQUENCE SET; Schema: base; Owner: steven
 --
 
-SELECT pg_catalog.setval('base.orders_order_id_seq', 1, false);
+SELECT pg_catalog.setval('base.orders_order_id_seq', 11, true);
 
 
 --
@@ -454,14 +548,14 @@ SELECT pg_catalog.setval('base.owner_owner_id_seq', 1, false);
 -- Name: payment_info_payment_info_id_seq; Type: SEQUENCE SET; Schema: base; Owner: steven
 --
 
-SELECT pg_catalog.setval('base.payment_info_payment_info_id_seq', 1, false);
+SELECT pg_catalog.setval('base.payment_info_payment_info_id_seq', 3, true);
 
 
 --
 -- Name: publisher_publisher_id_seq; Type: SEQUENCE SET; Schema: base; Owner: steven
 --
 
-SELECT pg_catalog.setval('base.publisher_publisher_id_seq', 1, false);
+SELECT pg_catalog.setval('base.publisher_publisher_id_seq', 4, true);
 
 
 --
